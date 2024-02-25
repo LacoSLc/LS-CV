ee66a2403634c72f9eb54dbe59f93068ea0873a5
LacoSLc <142248617+LacoSLc@users.noreply.github.com>
2024-02-25 21:18:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,6 @@ window.onscroll = () => {
 
         if (top >= offset && top < offset + height) {
             sec.classList.add('show-animate');
-
         }
 
         else{
@@ -37,6 +36,8 @@ window.onscroll = () => {
 
 // SECTION 3 GALLERY
 
+// FISHING
+
 let fishButton = document.querySelector('#fishButton');
 let closer1 = document.querySelector('#closer1');
 let galleryFish = document.querySelector("#wrapperFish");
@@ -46,13 +47,12 @@ const galleryEventF = (gallery, close, open) => {
     closer.style.display = close;
     fishButton.style.display = open;
 };
-
 fishButton.addEventListener('click', () => galleryEventF("flex", "block", "none"));
 closer1.addEventListener('click', () => galleryEventF("none", "none", "block"));
 
+// ARCHERY
 
-
-let archeryhButton = document.querySelector('#archeryButton');
+let archeryButton = document.querySelector('#archeryButton');
 let closer2 = document.querySelector('#closer2');
 let galleryArchery = document.querySelector("#wrapperArchery");
 
@@ -61,9 +61,68 @@ const galleryEventA = (gallery, close, open) => {
     archeryButton.style.display = open;
 };
 
-archeryhButton.addEventListener('click', () => galleryEventA("flex", "block", "none"));
+archeryButton.addEventListener('click', () => galleryEventA("flex", "block", "none"));
 closer2.addEventListener('click', () => galleryEventA("none", "none", "block"));
 
 
 
 
+// SECTION $ PERCENTA HTML
+
+let htmlButton = document.querySelector('#html');
+// let closer1 = document.querySelector('#closer1');
+let galleryHtml = document.querySelector("#perhtml");
+
+const galleryEventH = (gallery, close, open) => {
+    galleryHtml.style.display = gallery;
+    // closer.style.display = close;
+    htmlButton.style.display = open;
+};
+
+htmlButton.addEventListener('click', () => galleryEventH("flex", "block",));
+// closer1.addEventListener('click', () => galleryEventF("none", "none", "block"));
+
+// SECTION $ PERCENTA CSS
+
+let cssButton = document.querySelector('#css');
+// let closer1 = document.querySelector('#closer1');
+let galleryCss = document.querySelector("#percss");
+
+const galleryEventC = (gallery, close, open) => {
+    galleryCss.style.display = gallery;
+    // closer.style.display = close;
+    htmlButton.style.display = open;
+};
+
+cssButton.addEventListener('click', () => galleryEventC("flex", "block",));
+// closer1.addEventListener('click', () => galleryEventF("none", "none", "block"));
+
+// SECTION $ PERCENTA JS
+
+let jsButton = document.querySelector('#js');
+// let closer1 = document.querySelector('#closer1');
+let galleryJs = document.querySelector("#perjs");
+
+const galleryEventJS = (gallery, close, open) => {
+    galleryJs.style.display = gallery;
+    // closer.style.display = close;
+    htmlButton.style.display = open;
+};
+
+jsButton.addEventListener('click', () => galleryEventJS("flex", "block",));
+// closer1.addEventListener('click', () => galleryEventF("none", "none", "block"));
+
+// SECTION $ PERCENTA PYTHON
+
+let pythonButton = document.querySelector('#python');
+// let closer1 = document.querySelector('#closer1');
+let galleryPython = document.querySelector("#perpython");
+
+const galleryEventPy = (gallery, close, open) => {
+    galleryPython.style.display = gallery;
+    // closer.style.display = close;
+    htmlButton.style.display = open;
+};
+
+pythonButton.addEventListener('click', () => galleryEventPy("flex", "block",));
+// closer1.addEventListener('click', () => galleryEventF("none", "none", "block"));
