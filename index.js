e105cc649c0f3f3dd34a0ae7bc773c7c9d8669c1
LacoSLc <142248617+LacoSLc@users.noreply.github.com>
2024-05-06 22:07:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -64,65 +64,39 @@ const galleryEventA = (gallery, close, open) => {
 archeryButton.addEventListener('click', () => galleryEventA("flex", "block", "none"));
 closer2.addEventListener('click', () => galleryEventA("none", "none", "block"));
 
+// SECTION 4 
+
+// let btn = document.querySelector('.bttn');
+
+// function toggle() {
+//   btn.classList.toggle('active');
+// }
+
+// btn.addEventListener('click', toggle);
 
 
+// ---------------------------------------------------------------------------
+let links = document.querySelectorAll(".bttn");
 
-// SECTION $ PERCENTA HTML
-
-let htmlButton = document.querySelector('#html');
-// let closer1 = document.querySelector('#closer1');
-let galleryHtml = document.querySelector("#perhtml");
-
-const galleryEventH = (gallery, close, open) => {
-    galleryHtml.style.display = gallery;
-    // closer.style.display = close;
-    htmlButton.style.display = open;
-};
-
-htmlButton.addEventListener('click', () => galleryEventH("flex", "block",));
-// closer1.addEventListener('click', () => galleryEventF("none", "none", "block"));
-
-// SECTION $ PERCENTA CSS
-
-let cssButton = document.querySelector('#css');
-// let closer1 = document.querySelector('#closer1');
-let galleryCss = document.querySelector("#percss");
-
-const galleryEventC = (gallery, close, open) => {
-    galleryCss.style.display = gallery;
-    // closer.style.display = close;
-    htmlButton.style.display = open;
-};
-
-cssButton.addEventListener('click', () => galleryEventC("flex", "block",));
-// closer1.addEventListener('click', () => galleryEventF("none", "none", "block"));
-
-// SECTION $ PERCENTA JS
-
-let jsButton = document.querySelector('#js');
-// let closer1 = document.querySelector('#closer1');
-let galleryJs = document.querySelector("#perjs");
-
-const galleryEventJS = (gallery, close, open) => {
-    galleryJs.style.display = gallery;
-    // closer.style.display = close;
-    htmlButton.style.display = open;
-};
-
-jsButton.addEventListener('click', () => galleryEventJS("flex", "block",));
-// closer1.addEventListener('click', () => galleryEventF("none", "none", "block"));
-
-// SECTION $ PERCENTA PYTHON
-
-let pythonButton = document.querySelector('#python');
-// let closer1 = document.querySelector('#closer1');
-let galleryPython = document.querySelector("#perpython");
-
-const galleryEventPy = (gallery, close, open) => {
-    galleryPython.style.display = gallery;
-    // closer.style.display = close;
-    htmlButton.style.display = open;
-};
-
-pythonButton.addEventListener('click', () => galleryEventPy("flex", "block",));
-// closer1.addEventListener('click', () => galleryEventF("none", "none", "block"));
+// loop through all the links
+links.forEach(l => {
+  // add click listeners
+  l.addEventListener("click", (e) => {
+    e.preventDefault();
+    let currentTab = document.querySelector(`.wrappPer${l.href.substr(l.href.indexOf("#"))}`);
+    
+    if(currentTab.classList.contains("hide")){
+      // add hide class to all the tab content
+      document.querySelectorAll(".wrappPer").forEach(t => {
+        t.classList.add("hide");
+      });
+      // remove hide class from the tab content which's id matches with the current href
+      currentTab.classList.remove("hide");
+    }else{
+     // add hide class to all the tab content
+      document.querySelectorAll(".wrappPer").forEach(t => {
+        t.classList.add("hide");
+      });
+    }
+  })
+})
