40e1afb109687df950566e24ad431185059ac229
LacoSLc <142248617+LacoSLc@users.noreply.github.com>
2025-05-08 18:39:32 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -125,5 +125,12 @@ for (var i = 0; i < btns.length; i++) {
   });
 }
 
+var $ = (s, o = document) => o.querySelector(s);
 
+$(".dl").addEventListener("click", function() {
+  this.classList.add("run");
+  setTimeout(() => this.classList.add("done"), 4000);
+  setTimeout(() => this.classList.remove("done"), 5500);
+  setTimeout(() => this.classList.remove("run"), 5500);
+});
 
