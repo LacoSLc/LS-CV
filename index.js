086cfeccba537e32b03a4ac082cf5232d4a04585
LacoSLc <142248617+LacoSLc@users.noreply.github.com>
2025-05-16 10:35:45 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ window.onscroll = () => {
 }
 
 
+
 // SECTION 3 GALLERY
 
 // FISHING
@@ -134,3 +135,38 @@ $(".dl").addEventListener("click", function() {
   setTimeout(() => this.classList.remove("run"), 5500);
 });
 
+
+
+// Side navigator
+
+let mainNavLinks = document.querySelectorAll(".sideLight");
+let mainSections = document.querySelectorAll("section");
+
+let lastId;
+let cur = [];
+
+// This should probably be throttled.
+// Especially because it triggers during smooth scrolling.
+// https://lodash.com/docs/4.17.10#throttle
+// You could do like...
+// window.addEventListener("scroll", () => {
+//    _.throttle(doThatStuff, 100);
+// });
+// Only not doing it here to keep this Pen dependency-free.
+
+window.addEventListener("scroll", event => {
+  let fromTop = window.scrollY;
+
+  mainNavLinks.forEach(link => {
+    let section = document.querySelector(link.hash);
+
+    if (
+      section.offsetTop <= fromTop  &&
+      section.offsetTop + section.offsetHeight > fromTop
+    ) {
+      link.classList.add("current");
+    } else {
+      link.classList.remove("current");
+    }
+  });
+});
